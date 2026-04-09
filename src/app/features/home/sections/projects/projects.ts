5f20500e7b2681b557641f3d85adc981b562fa9f
Julia Keller <juliakeller.vk@gmail.com>
2026-04-09 23:10:25 +0200

    projects section cards added

--- src/app/features/home/sections/projects/projects.ts
+++ src/app/features/home/sections/projects/projects.ts
@@ -1,12 +1,26 @@
+
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Heading } from "../../../../shared/components/heading/heading";
 import { Section } from '../../../../shared/components/section/section';
 import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-projects',
-  imports: [Heading, Section, TranslatePipe],
+  imports: [CommonModule, Heading, Section, TranslatePipe],
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
-export class Projects {}
+export class Projects {
+  tabs = [
+    { id: 'Card1', label: 'Karte 1', content: 'Inhalt 1' },
+    { id: 'Card2', label: 'Karte 2', content: 'Inhalt 2' },
+    { id: 'Card3', label: 'Karte 3', content: 'Inhalt 3' },
+    { id: 'Card4', label: 'Karte 4', content: 'Inhalt 4' },
+  ];
+  activeTab = this.tabs[0].id;
+
+  setActiveTab(tabId: string) {
+    this.activeTab = tabId;
+  }
+}
