868762b324806231c34f7160e1b0ace8938f076b
Julia Keller <juliakeller.vk@gmail.com>
2026-04-17 02:36:00 +0200

    container width bug fixed

--- src/app/shared/components/skill-icon/skill-icon.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-skill-icon',
-  imports: [],
-  templateUrl: './skill-icon.html',
-  styleUrl: './skill-icon.scss',
-})
-export class SkillIcon {}
